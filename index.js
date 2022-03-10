25d5a18cc4651f66eefb442211df34f4622eba37
Hanin Mez <haninjamalmzher@gmail.com>
2022-03-10 18:43:40 +0100

    done

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { findOneAndUpdate } = require('./models/Recipe.model');
+// const { modelName } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -17,7 +19,45 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-  })
+    let chocCake = {
+      title:'Chocolate cake',
+      level: 'Easy Peasy',
+      ingredients:["1/2 cup light brown sugar",
+    "1 large egg",
+    "2 tablespoons milk",
+    "1 1/4 teaspoons vanilla extract",
+    "2 cups semisweet chocolate chips" ],
+    cusine:'portuguese',
+    dishType:'dessert',
+    image:"https://www.istockphoto.com/de/foto/süße-hausgemachte-dunkle-schokolade-torte-gm904337728-249400791",
+    duration: 30, 
+    creator: "Daiany"};
+  
+    Recipe.create(chocCake)
+    .then((result)=>{console.log(result.title)})
+    .then(()=>{
+      return Recipe.insertMany(data).then((allRecipe)=>{
+        for(let i= 0; i < allRecipe.length; i++){
+          console.log(allRecipe[i].title);
+        }})
+    })
+    .then(() => {
+      return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration:100});
+       }).then((updated)=>{
+         console.log('updated', updated) 
+         return Recipe.deleteOne({title:"Carrot Cake"});
+       }).then(()=> {console.log('deleted')})
+       
+       
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+ 
+  
+    })
+  //.finally(()=>{
+  //   mongoose.connection.close();
+  //  }).then(()=>{
+  //    console.log("closed!")
+  //  })
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,8 +1,67 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
+
+// function arrLengthFactory(strArray, length) {
+//   for (let i = strArray.length; i < length; i++) {
+//     strArray.push(defaultPadding);
+//   }
+//   return strArray;
+// }
+
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title :{
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef' , 'UltraPro Chef'],
+    },
+      ingredients: {
+      type: [String],
+    },
+      cusine:{
+      type: String,
+      // required: true,
+    },
+  
+      dishType: {
+      type: String,
+      enum: ["breakfast","main_course", "soup", "snack", "drink", "dessert","other"],
+      //set: arrLengthFactory(enum, 7),
+      // validate: {
+      //   validator: (levelLength) => {
+      //     return levelLength.length === 7;
+      //   },
+      //   message : "recipe should have 7 dish types!"
+      // },
+      // default: [],
+      },
+
+      image: {
+        type: String,
+        default: "https://images.media-allrecipes.com/images/75131.jpg",
+      },
+
+      duration: {
+        type: Number,
+        min: 0,
+      },
+
+      creator: {
+        type: String,
+      },
+
+      created: {
+        type: Date,
+        default: Date.today,
+      }
+  
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
